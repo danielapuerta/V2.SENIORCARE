3279dfa86c999cf57072383b14bcc4867e67d24c
danielapuerta <danielapuerta151@gmail.com>
2022-10-05 11:51:26 +0100

    installing bcrypt

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,12 @@ module.exports = function (app){
    app.get('/', function(req, res){
       res.render('login');
    });
+   app.post('/', (req, res) => {
+      // Insert Login Code Here
+      let nurseCode = req.body.nurseCode;
+      let password = req.body.password;
+      res.send(`nurseCode: ${nurseCode} password: ${password}`);
+    });
 
    
 };
