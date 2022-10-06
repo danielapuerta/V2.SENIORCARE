e77dee69f279a111c3851348197d8ebc3f5e5149
danielapuerta <danielapuerta151@gmail.com>
2022-10-06 11:44:15 +0100

    create middleware function checkDuplicateNurseCode

---
+++ middleware/verifySignUp.js
@@ -0,0 +1,28 @@
+const db = require("../models");
+const User = db.user;
+
+//to verify a SignUp (register) action, we need the following function: checkDuplicateNurseCode()
+// check if nurseCode is duplicated or not
+
+checkDuplicateNurseCode = (req, res, next) => {
+    //nurseCode
+    User.findOne({
+        where: {
+            nurseCode: req.body.nurseCode
+        }
+    }).then(user => {
+        if(user){
+            res.status(400).send({
+                message: "Failed! nurseCode is already in use!"
+            });
+            return;
+        }
+        next();
+    });
+}
+
+const verifySignUp = {
+    checkDuplicateNurseCode : checkDuplicateNurseCode
+};
+
+module.exports = verifySignUp;
